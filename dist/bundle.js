3855ea55fed08d22d249aab3afff71cf120a600c
Chema De la Cruz Sanchez <chemadelacruzsanchez@MBP-de-Chema-2.fritz.box>
2022-04-30 17:14:06 +0200

    read function

--- dist/bundle.js
+++ dist/bundle.js
@@ -7,6 +7,106 @@ async function connect() { // Connect Metamask
     }
 }
 
+async function read() { // used to read from the Blockchain
+    const contractAddress = "0x5FbDB2315678afecb367f032d93F642f64180aa3"; // Address from SimpleStorage.sol deployment done when local blockchain was executed (you can get any other contract)
+    const abi = [  // ABI from SmarContract Artifacts || best pratice -> include in a file and import it
+        {
+          "inputs": [
+            {
+              "internalType": "string",
+              "name": "_name",
+              "type": "string"
+            },
+            {
+              "internalType": "uint256",
+              "name": "_favoriteNumber",
+              "type": "uint256"
+            }
+          ],
+          "name": "addPerson",
+          "outputs": [],
+          "stateMutability": "nonpayable",
+          "type": "function"
+        },
+        {
+          "inputs": [
+            {
+              "internalType": "string",
+              "name": "",
+              "type": "string"
+            }
+          ],
+          "name": "nameToFavoriteNumber",
+          "outputs": [
+            {
+              "internalType": "uint256",
+              "name": "",
+              "type": "uint256"
+            }
+          ],
+          "stateMutability": "view",
+          "type": "function"
+        },
+        {
+          "inputs": [
+            {
+              "internalType": "uint256",
+              "name": "",
+              "type": "uint256"
+            }
+          ],
+          "name": "people",
+          "outputs": [
+            {
+              "internalType": "uint256",
+              "name": "favoriteNumber",
+              "type": "uint256"
+            },
+            {
+              "internalType": "string",
+              "name": "name",
+              "type": "string"
+            }
+          ],
+          "stateMutability": "view",
+          "type": "function"
+        },
+        {
+          "inputs": [],
+          "name": "retrieve",
+          "outputs": [
+            {
+              "internalType": "uint256",
+              "name": "",
+              "type": "uint256"
+            }
+          ],
+          "stateMutability": "view",
+          "type": "function"
+        },
+        {
+          "inputs": [
+            {
+              "internalType": "uint256",
+              "name": "_favoriteNumber",
+              "type": "uint256"
+            }
+          ],
+          "name": "store",
+          "outputs": [],
+          "stateMutability": "nonpayable",
+          "type": "function"
+        }
+      ];
+
+      // Now: we connect to the Blockchain and execute the transaction
+      const provider = new ethers.providers.Web3Provider(window.ethereum);
+      const signer = provider.getSigner(); // signer: MetaMask account
+      const contract = new ethers.Contract(contractAddress, abi, signer); // instantiate Contract with connected MetaMask account
+      const value = await contract.retrieve(); // returns stored value
+      console.log(value.toString());
+}
+
 async function store() { // Store function from hardhat-simple-storage -> SimpleStorage.sol
    if (typeof window.ethereum !== "undefined") {
     // we need -> ADDRESS, ABI
@@ -115,9 +215,10 @@ async function store() { // Store function from hardhat-simple-storage -> Simple
    }
 }
 
-module.exports = {
+module.exports = { // export functions
     connect,
     store,
+    read,
 };
 
 },{"ethers":146}],2:[function(require,module,exports){
